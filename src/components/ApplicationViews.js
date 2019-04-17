1eff61ae9e98d7413af020f68cb782c1356ce0a7
Jordan Rosas <jordanrosas1203@gmail.com>
2019-04-17 15:53:23 -0500

    added the clouds

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,19 +1,20 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
+import Welcome from '../components/welcome/Welcome'
 
 export default class ApplicationViews extends Component {
 
   state = {
-    typeOne = 0,
-    typeTwo = 0,
-    typeThree = 0,
-    typeFour = 0,
-    typeFive = 0,
-    typeSix = 0,
-    typeSeven= 0,
-    typeEight = 0,
-    typeNine = 0,
-    userName = ''
+    typeOne :0,
+    typeTwo :0,
+    typeThree :0,
+    typeFour :0,
+    typeFive :0,
+    typeSix :0,
+    typeSeven:0,
+    typeEight :0,
+    typeNine :0,
+    userName :''
   }
 
   render() {
@@ -22,7 +23,7 @@ export default class ApplicationViews extends Component {
 
         <Route
           exact path="/" render={props => {
-            return null
+            return <Welcome />
           }}
         />
 
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
-
+import Cloud from '../assets/Clouds.png'
 export default class Welcome extends Component{
-  
+  render(){
+    return(
+      <>
+        <img className = "Cloud" src = {Cloud} alt = "cloud Image" />
+        <img className = "Cloud2" src = {Cloud} alt = "cloud Image" />
+        <img className = "Cloud3" src = {Cloud} alt = "cloud Image" />
+        <img className = "Cloud4" src = {Cloud} alt = "cloud Image" />
+      </>
+    )
+
+  }
 }
