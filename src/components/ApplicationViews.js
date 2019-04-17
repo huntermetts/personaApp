e958bcec4ea16c7b11cb4dba280d4765068276eb
Jordan Rosas <jordanrosas1203@gmail.com>
2019-04-17 15:13:51 -0500

    welcome folder and welcome component added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,6 +3,19 @@ import React, { Component } from "react";
 
 export default class ApplicationViews extends Component {
 
+  state = {
+    typeOne = 0,
+    typeTwo = 0,
+    typeThree = 0,
+    typeFour = 0,
+    typeFive = 0,
+    typeSix = 0,
+    typeSeven= 0,
+    typeEight = 0,
+    typeNine = 0,
+    userName = ''
+  }
+
   render() {
     return (
       <React.Fragment>
