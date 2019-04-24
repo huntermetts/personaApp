ec41ed192c9236a545f1a5a8314a05ea35149a3b
Hunter Metts <huntermettsdev@gmail.com>
2019-04-24 16:19:40 -0500

    Added mobile styles for page 1

--- src/components/Questions/PageOne.js
+++ src/components/Questions/PageOne.js
@@ -107,12 +107,119 @@ export default class PageOne extends Component
 
         />
 
+        {/* Question 11 */}
+        <p className="questions">11. Status and success play a large role in my life.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        />
+
+        {/* Question 12 */}
+        <p className="questions">12. In love the most important things âre being fair and behaving decently.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        />
+
+        {/* Question 13 */}
+        <p className="questions">13. I can readily adjust to the demands of a new situation.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 14 */}
+        <p className="questions">14. I'm on the side of marginal groups, people oppressed and discriminated against (refugees, third world children, minorities, etc).
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 15 */}
+        <p className="questions">15. Life is like a drama in which I'm both actor and spectator.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 16 */}
+        <p className="questions">16. For the sake of advancing my career I’m prepared to neglect my marriage, family, or friends.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 17 */}
+        <p className="questions">17. I’m often bothered by a bad conscience.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 18 */}
+        <p className="questions">18. I have the impression that the so-called "authorities" are incompetent  but I usually hesitate to take action against them.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 19 */}
+        <p className="questions">19. I like to let things run their course. Lots of times problems just take care of themselves.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 20 */}
+        <p className="questions">20. I have to know where I belong.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        />
+
+         {/* Question 21 */}
+        <p className="questions">21. I enjoy getting attention from other people and being in the limelight.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        />
+
+          {/* Question 22 */}
+          <p className="questions">22. I often suppress my anger.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        /> {/* Question 23 */}
+          <p className="questions">23. I have the feeling that I can never be completely fulfilled.
+        </p>
+          <Rating className="rating"
+          stop = {6}
+          initialRating = {1}
+
+        />
+
+         <div className="stepOneButtonContainer">
+            <button id = "WelcomeButton" type="button" className="btn text-dark welcomeButton" onClick={() => this.pushToPageOne()}>Next</button>
+         </div>
+
+
           <div  className="centerProgress">
               <div className="progressBar">
                   <Progress striped animated color="dark" value="20" />
               </div>
           </div>
         </div>
+
 
         <img className = "Cloud" src = {Cloud} alt = "cloud" />
         <img className = "Cloud2" src = {Cloud} alt = "cloud" />
